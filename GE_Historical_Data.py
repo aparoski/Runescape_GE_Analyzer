16bf4aaafecb625c24c08be422f540b5e74c6e56
Anton Paroski <Anton_Paroski@Alumni.baylor.edu>
2024-09-18 15:25:17 -0500

    Updates to data readin function to avoid database

--- GE_Historical_Data.py
+++ GE_Historical_Data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import pyodbc
 
 import datetime
 import time
@@ -10,27 +9,7 @@ from bs4 import BeautifulSoup as bs
 
 import json
 
-#database stuff ---------------------------------------------------------------
-conn = pyodbc.connect(r'Driver={SQL Server};'
-                     r'Server=Helios;'
-                     r'Database=Data_Science_Hangout;'
-                     r'Trusted_Connection=yes;')
-
-cursor = conn.cursor()
-
-SQL = """SELECT U.NAME_ID, U.NAME_URL, N.NAME 
-            FROM dbo.RUNESCAPE_ITEMS_URLS AS U 
-            INNER JOIN dbo.RUNESCAPE_ITEMS_NAMES AS N 
-                ON U.NAME_ID = N.NAME_ID"""
-
-Runescape_Items = pd.read_sql_query(SQL, conn)
-
-#database stuff ---------------------------------------------------------------
-
-conn.close()
-
 Runescape_Items = pd.read_csv("Runescape_Item_Names.csv")
-
 
 def epochms_to_dt(ms):
     s = int(ms) / 1000.0
@@ -39,13 +18,11 @@ def epochms_to_dt(ms):
     
     return(date)
     
-
-
 #crawl through Wiki API to gather historical price data as far back as 2008
     
 ID_df_list = []
 
-for i in Runescape_Items["NAME_ID"]:
+for i in Runescape_Items["Name_ID"]:
     
     req = requests.get('https://api.weirdgloop.org/exchange/history/rs/all?id=' + 
                        str(i).strip() + 
@@ -77,31 +54,12 @@ for i in Runescape_Items["NAME_ID"]:
     
     df['date']= pd.to_datetime(df['date'])
     
-    df = df[df["date"] < datetime.datetime.strptime('2022-06-11', '%Y-%m-%d')]
+    #df = df[df["date"] < datetime.datetime.strptime('2024-09-18', '%Y-%m-%d')]
     
     ID_df_list.append(df)
+
+    print(i + " prices have been loaded")
     
 GE_data_history = pd.concat(ID_df_list)
 
-#database stuff ---------------------------------------------------------------
-
-
-
-conn = pyodbc.connect(r'Driver={SQL Server};'
-                     r'Server=Helios;'
-                     r'Database=Data_Science_Hangout;'
-                     r'Trusted_Connection=yes;')
-
-cursor = conn.cursor()
-    
-for index, row in GE_data_history.iterrows():
-
-    query = "INSERT INTO dbo.RUNESCAPE_ITEMS_PRICES (NAME_ID, Price, Date) VALUES ('{}', '{}', '{}')".format(row["id"],
-                                                                                                             row["price"],
-                                                                                                             row["date"])
-    cursor.execute(query)
-
-conn.commit()
-conn.close()
-
-#database stuff ---------------------------------------------------------------
+GE_data_history.to_csv("ID_Prices.csv")
