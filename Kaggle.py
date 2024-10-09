88a45f2e3736a808ec5d9f3e0ab260b84f8d7c8d
Anton Paroski <Anton_Paroski@Alumni.baylor.edu>
2024-10-09 14:23:12 -0500

    _cli version of dataset code appears to work

--- Kaggle.py
+++ Kaggle.py
@@ -3,16 +3,32 @@ import config
 import subprocess
 import os, json, subprocess
 
-api = kaggle.api
-# there should only be one dataset for runescape written by myself...for now
-datasets = kaggle.api.dataset_list(search = "aparoski/Runescape")
-
-Runescape_Dataset = datasets[0]
 
 
 #need the most recently updated folder which should contain the newly updated dataset. 
-data_files = os.listdir(config.upper_dir)
-paths = [os.path.join(config.upper_dir, folder) for folder in data_files]
-oldest_folder = max(paths, key = os.path.getctime)
 
-print(oldest_folder)
+def most_recent_local_dataset():
+    """returns the latest updated runescape dataset folder path
+    as a string"""
+    data_files = os.listdir(config.upper_dir)
+    paths = [os.path.join(config.upper_dir, folder) for folder in data_files]
+    recent_folder = max(paths, key = os.path.getctime)
+
+    return(recent_folder)
+
+
+#def runescape_dataset_upload(data):
+
+# there should only be one dataset for runescape written by myself...for now
+datasets = kaggle.api.dataset_list(search = "aparoski/Runescape")
+
+Rune_Dat = datasets[0]
+
+Rune_Dat
+
+Rune_Dat_Files = kaggle.api.dataset_create_version_cli()
+
+
+
+if __name__ == "__main__":
+    print()
